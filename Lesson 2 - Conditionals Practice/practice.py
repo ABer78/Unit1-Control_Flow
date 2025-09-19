7104756f070d28d243ab3967def0c5133550cf0d
ABer78 <arpiberb@gmail.com>
2025-09-19 17:21:19 -0400

    ih

--- Lesson 2 - Conditionals Practice/practice.py
+++ Lesson 2 - Conditionals Practice/practice.py
@@ -2,19 +2,23 @@ age = input("Enter your age:")
 if age:
     age = int(age)
 
-rating = input("Enter the movie rating:")
+rating = input("Enter the movie rating (G, PG, PG-13, R):")
 
-if rating == "G":
-    print("You can watch this movie")
+if age == "":
+    print("Please enter your age")
+elif rating == "":
+    print("Please enter the movie rating")
+elif rating == "G":
+    print("APPROVED: You can watch this movie")
 elif rating == "PG" and age >= 6:
-    print("You can watch this movie")
+    print("APPROVED: You can watch this movie")
 elif rating == "PG" and age < 6:
-    print("You can not watch this movie")
+    print("DENIED: You can not watch this movie")
 elif rating == "PG-13" and age >= 13:
-    print("You can watch this movie")
+    print("APPROVED: You can watch this movie")
 elif rating == "PG-13" and age < 13:
-    print("You can not watch this movie")
+    print("DENIED: You can not watch this movie")
 elif rating == "R" and age >= 17:
-    print("You can watch this movie")
+    print("APPROVED: You can watch this movie")
 elif rating == "R" and age < 17:
-    print("You can not watch this movie")
+    print("DENIED:You can not watch this movie")
