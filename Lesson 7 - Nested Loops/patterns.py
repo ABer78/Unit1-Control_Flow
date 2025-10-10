f3497b6bdbcdf847d73843323a2235c31f4a62ed
ABer78 <arpiberb@gmail.com>
2025-10-10 08:38:11 -0400

    10\10/25

--- Lesson 7 - Nested Loops/patterns.py
+++ Lesson 7 - Nested Loops/patterns.py
@@ -8,3 +8,13 @@
 # Row 4: 0 spaces, 9 star
 
 rows = 5
+# Step 1: create an outer loop for the row
+for i in range(rows):
+    # Step 2: print the spaces
+    for j in range(rows - i - 1):
+        print(" ", end="")
+    # Step 3: print the stars 2*i+1
+    for k in range(2 * i + 1):
+        print("*", end="")
+    # Step 4: print a new line
+    print()
